f41d8c5ece701a83560b7ce4796cafb454be844a
Dany <dany@MacBookAir.local>
2018-05-05 18:59:01 +0200

    CI: Replace cppunit by crpcut

--- .integration/travis-install-linux.sh
+++ .integration/travis-install-linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+add-apt-repository ppa:jonlar/crpcut
 apt-get update
 
 # Global packages
@@ -9,7 +10,7 @@ apt-get install -y git cmake g++ clang
 apt-get install -y libgtksourceviewmm-3.0-dev freeglut3 freeglut3-dev mesa-common-dev libglu1-mesa-dev libglew-dev libglfw3-dev libdw-dev
 
 # Unit tests dependencies
-apt-get install -y libcppunit-dev libglm-dev llvm-4.0-tools
+apt-get install -y crpcut crpcut-dev libglm-dev llvm-4.0-tools
 ln -s /usr/bin/llvm-symbolizer-4.0 /usr/bin/llvm-symbolizer
 
 # Coveralls dependencies
